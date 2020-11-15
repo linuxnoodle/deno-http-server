8c4a888df81c2122765dce5da75e0ea131c6667b
Farhan Sheikh <ipadnoodle@gmail.com>
2020-11-14 22:26:44 -0500

    Update code

--- server.ts
+++ server.ts
@@ -1,27 +1,37 @@
-// Imports 'http' and 'fs' from the deno std libraries.
-import { serve, ServerRequest } from "https://deno.land/std/http/server.ts";
+// Imports deno http libraries.
+import {serve, ServerRequest} from "https://deno.land/std/http/server.ts";
 
+// Starts server.
 const PORT = 80;
-const server = serve({ port: PORT });
+const server = serve({port: PORT});
 
 console.log(`Listening on http://localhost:${PORT}.`);
 
+// Runs on every request.
 for await (const request : ServerRequest of server){
+    // Checks request type.
     if (request.method === "GET"){
+        // Returns associated file.
         try {
             if (request.url === "/"){
                 request.respond({status: 200, body: await Deno.readFile("./website/index.html")});
-                await request.finalize();
             } else {
-                request.respond({status: 200, body: await Deno.readFile("./website/" + request.url.substring(1))});
-                await request.finalize();
+                request.respond({status: 200, body: await Deno.readFile("./website" + request.url)})
             }
-        } catch (e) {
+        } catch (e){
             request.respond({status: 404, body: await Deno.readFile("./website/404.html")})
-            await request.finalize();
         }
-    } else {
-        request.respond({status: 405});
-        await request.finalize();
+    } else if (request.method === "POST"){
+        // Checks if password provided is correct.
+        try {
+            const buffer: Uint8Array = await Deno.readAll(request.body);
+            if (new TextDecoder("utf-8").decode(buffer) == "password"){
+                request.respond({status: 200, body: "good job"});
+            } else {
+                request.respond({status: 200, body: "bad job"});
+            }
+        } catch (e){
+            request.respond({status: 405, body: "There was an error in handling your message."});
+        }
     }
 }
